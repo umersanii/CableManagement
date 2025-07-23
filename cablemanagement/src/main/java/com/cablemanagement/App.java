682588f4ede2924071b8543d3ac9c191e3e4b47f
Umer <iamumersani@gmail.com>
2025-07-23 13:29:26 +0500

    feat: Refactor App class to extend Application and launch JavaFX application

--- cablemanagement/src/main/java/com/cablemanagement/App.java
+++ cablemanagement/src/main/java/com/cablemanagement/App.java
@@ -1,14 +1,27 @@
 package com.cablemanagement;
 
 import com.cablemanagement.views.signin_page;
+import com.cablemanagement.views.home_page;
 
-public class App {
-    public static void main(String[] args) throws Exception {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
-        // initalize database
+        // initialize database
         config.database.connect(null, null, null);
 
-        signin_page.main(args);
+        // Launch JavaFX application
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Load home page temporarily
+        com.cablemanagement.views.home_page.getHomeScene();
+        primaryStage.setScene(com.cablemanagement.views.home_page.getHomeScene());
+        primaryStage.setTitle("Cable Management - Home");
+        primaryStage.show();
     }
 }
